22d7f8b6a0796737c19274d5118ded1e3f33bf4f
josebohorquez1 <yassirbohorquez@gmail.com>
2024-10-11 03:05:02 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/users').then(() => console.log("MongoDB connected.")).catch(err => console.error(err));
+app.get('/', (req, res) => {res.send("Welcome to the project!");});
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
