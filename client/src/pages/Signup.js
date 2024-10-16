0d59eac69b1d02dad5df76d03bb74901b9677841
josebohorquez1 <yassirbohorquez@gmail.com>
2024-10-15 20:11:19 -0400

    Created user account creation page.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,7 +2,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Signup = () => {
     return (
-        <h1>This is the sign up page.</h1>
+        <div className="flex-grow-1 d-flex justify-content-center align-items-center">
+            <main className="container">
+            <h1 className="bg-primary text-white text-center p-4">Sign Up</h1>
+            <form className="bg-light p-5 rounded shadow" style={{ maxWidth: '500px', margin: 'auto' }}>
+            <div className="mb-3">
+                <label htmlFor="fname" classname="form-label">First Name:</label>
+                <input type="text" name="fname" id="fname" className="form-control" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="lname" className="form-label">Last Name:</label>
+                <input type="text" name="lname" id="lname" className="form-control" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email:</label>
+                <input type="email" name="email" id="email" className="form-control" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="uname" className="form-label">User Name:</label>
+                <input type="text" name="uname" id="uname" className="form-control" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password:</label>
+                <input type="password" name="password" id="password" className="form-control" required />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="confirmpassword" className="form-label">Confirm Password:</label>
+                <input type="password" name="confirmpassword" id="confirmpassword" className="form-control" required />
+        </div>
+            <button type="submit" className="btn btn-primary w-100">Create Account</button>
+            </form>
+            </main>
+        </div>
     );
 };
 
